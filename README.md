41d6cda780ef7d79647977aed179e009e084d7f1
Nelson Mungai <mungainelle96@gmail.com>
2023-10-26 13:56:35 +0300

    Deleted some pages from prev version

--- README.md
+++
@@ -1,5 +0,0 @@
-# DMK
-
-<!-- changed the Appointment button to discover that leads to the portfolio.html -->
-
-<!-- able to implement the back to top featuresa -->
--- contact/contact.js
+++
@@ -1,7 +0,0 @@
-hamburger_menu=document.querySelector('#nav-bar .nav-bar .nav-list .hamburger')
-nav_list=document.querySelector('#nav-bar .nav-bar .nav-list')
-
-hamburger_menu.addEventListener('click',()=>{
-    hamburger_menu.classList.toggle('active')
-    nav_list.classList.toggle('open')
-})
